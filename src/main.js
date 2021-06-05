34e78a096e09e3575d9a2dadc1424c5677986d24
Alejo Arue Ocampo <a.o.alejo@gmail.com>
2021-06-05 02:51:53 -0300

    created rolls view

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
