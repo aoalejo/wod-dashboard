8888dbdc5abc927e547fe34c65785a6bae2f9766
Alejo Arue Ocampo <a.o.alejo@gmail.com>
2021-06-08 15:47:04 -0300

    one step closer

--- src/components/Sheet/Footer.vue
+++ src/components/Sheet/Footer.vue
@@ -1,11 +1,16 @@
 <template>
   <b-alert
-    align-v="stretch"
-    class="d-flex row p-0 m-0 h-100"
+    class="d-flex row p-1 m-0 h-100"
     align-self="stretch"
     show
-    :variant="info.color"
+    :style="'background: ' + info.color"
   >
+    <b-badge
+      class="d-flex flex-fill text-center justify-content-center"
+      style="height: max-content"
+    >
+      {{ health.name }}
+    </b-badge>
     <b-form-select
       v-for="i in health.max"
       :key="'HealthValue' + i"
@@ -15,20 +20,7 @@
       class="mt-0"
     ></b-form-select>
   </b-alert>
-</template>
- "health": {
-    "max": 7,
-    "damage": [
-      1,
-      3
-    ],
-    "types": [
-      "Contundente",
-      "No letal",
-      "Letal",
-      "Agravado"
-    ]
-  }
+</template> 
 <script>
 export default {
   name: "Footer",
@@ -48,5 +40,13 @@ export default {
   border-bottom-right-radius: 0.25rem;
   border-bottom-left-radius: 0rem;
   border-top-left-radius: 0rem;
+}
+
+.custom-select-sm {
+  height: calc(1em + 0.5rem + 2px);
+  padding-top: 0;
+  padding-bottom: 0;
+  padding-left: 0.5rem;
+  font-size: 0.875rem;
 }
 </style>
